33829bded20cb411cf03e28957d4a8417eb2ab03
Saptarshi Pal <saptarshi.pal.2002@gmail.com>
2026-01-11 19:39:59 +0530

    Added logging for GAN training process

---
+++ src/core/Logger.cpp
@@ -0,0 +1,23 @@
+#include "Logger.hpp"
+
+namespace mcgan {
+namespace core {
+
+// Logger class is fully implemented in the header file with inline methods.
+// The static members are also initialized inline in the header.
+// 
+// This file serves as a placeholder for any future non-inline implementations.
+
+// If you need to add utility functions or additional logging features, add them here:
+
+/*
+// Example: Helper function to configure logger from config file
+void configureLoggerFromFile(Logger& logger, const std::string& config_file) {
+    // Parse config and set logger properties
+    logger.setLevel(LogLevel::INFO);
+    logger.enableFileOutput("app.log");
+}
+*/
+
+} // namespace core
+} // namespace mcgan
